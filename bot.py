2eb0cc34e78b3d90c6461c683b37f77d8af60bf6
Ssb1382 <sajjad.saboor.8282@gmail.com>
2025-08-10 16:17:31 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,18 +2,15 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 
-# ========== تنظیمات ==========
-BOT_TOKEN = "8379742321:AAEef6P1oYZr0r-Ud8Yt1DaUBCo_7U7HhQI"  # اینجا توکن ربات که از BotFather گرفتی رو بگذار
+BOT_TOKEN = "8379742321:AAEef6P1oYZr0r-Ud8Yt1DaUBCo_7U7HhQI"
 MY_ID = 1905094596
 
 async def forward_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     sender = update.message.from_user
     if update.message.text:
-        await context.bot.send_message(
-            chat_id=MY_ID,
-            text=f"پیام از {sender.first_name}:
-{update.message.text}"
-        )
+        msg = "پیام از {}:
+{}".format(sender.first_name, update.message.text)
+        await context.bot.send_message(chat_id=MY_ID, text=msg)
     else:
         await context.bot.forward_message(
             chat_id=MY_ID,
